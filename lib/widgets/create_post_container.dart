43c7bea7eb4e50c26fe6ecb205cca853c5ab3e32
Chethan-170 <chethanpoojary170@gmail.com>
2023-06-07 15:45:38 +0530

    mobile design completed

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:facebook_clone_flutter/models/models.dart';
 import 'package:facebook_clone_flutter/widgets/widgets.dart';
 import 'package:flutter/material.dart';
---
+++ lib/widgets/custom_tab_bar.dart
@@ -0,0 +1,49 @@
+import 'package:facebook_clone_flutter/config/palette.dart';
+import 'package:flutter/material.dart';
+
+class CustomTabBar extends StatelessWidget {
+  final List<IconData> icons;
+  final int selectedIndex;
+  final Function onTap;
+
+  const CustomTabBar({
+    super.key,
+    required this.icons,
+    required this.selectedIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      indicatorPadding: EdgeInsets.zero,
+      indicator: const BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: Palette.facebookBlue,
+            width: 3.0,
+          ),
+        ),
+      ),
+      tabs: icons
+          .asMap()
+          .map(
+            (i, e) => MapEntry(
+              i,
+              Tab(
+                icon: Icon(
+                  e,
+                  color: i == selectedIndex
+                      ? Palette.facebookBlue
+                      : Colors.black45,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          )
+          .values
+          .toList(),
+      onTap: (index) => onTap(index),
+    );
+  }
+}
